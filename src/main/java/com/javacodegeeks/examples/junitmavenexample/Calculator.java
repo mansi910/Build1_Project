e749b6aa75b1515f6b4a5a58278dbc6bf1ca1e2a
hellno-7 <hellno-7@users.noreply.github.com>
2016-06-26 20:36:56 +0530

    Update Calculator.java

--- src/main/java/com/javacodegeeks/examples/junitmavenexample/Calculator.java
+++ src/main/java/com/javacodegeeks/examples/junitmavenexample/Calculator.java
@@ -22,7 +22,6 @@ public class Calculator implements ICalculator {
 		}
 
 		return a / b;	//returns division of 2 numbers
-		?
 		/*System.out.println("Hello World");
 		System.out.println("Hello World");*/
 	}
