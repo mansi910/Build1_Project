631e182f6ced5097c64d06ddb18de18870c6353f
hellno-7 <hellno-7@users.noreply.github.com>
2016-06-29 15:50:02 +0530

    Update Calculator.java

--- src/main/java/com/javacodegeeks/examples/junitmavenexample/Calculator.java
+++ src/main/java/com/javacodegeeks/examples/junitmavenexample/Calculator.java
@@ -4,28 +4,26 @@ public class Calculator implements ICalculator {
 
 	public int sum(int a, int b) {
 		return a + b;	//returns sum of 2 numbers
-		return "Hello!";
 	}
 
 	public int subtraction(int a, int b) {
 		return a - b;	//returns difference of 2 numbers
-		return "Hallo!";
 	}
 
 	
 	public int multiplication(int a, int b) {
 		return a * b;	//returns product of 2 numbers
-		return "Bonjour!";
 	}
 
 	
 	public int divison(int a, int b) throws Exception {
 		if (b == 0) {
-			throw new ArithmeticException("Divider can't be zero");
+			throw new Exception("Divider can't be zero");
 		}
 
 		return a / b;	//returns division of 2 numbers
 	}
+
 	
 	public boolean equalIntegers(int a, int b) {
 		boolean result = false;
