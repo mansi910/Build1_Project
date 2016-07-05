a57cc21387b0a7de6dc90fb3d38fde7392d809fa
hellno-7 <hellno-7@users.noreply.github.com>
2016-07-05 13:45:51 +0530

    Update ICalculator.java

--- src/main/java/com/javacodegeeks/examples/junitmavenexample/ICalculator.java
+++ src/main/java/com/javacodegeeks/examples/junitmavenexample/ICalculator.java
@@ -7,7 +7,7 @@ public interface ICalculator {
 
 	int multiplication(int a, int b);
 
-	int divison(int a, int b) throws ArithmeticException;
+	int divison(int a, int b);
 
 	boolean equalIntegers(int a, int b);
 }
